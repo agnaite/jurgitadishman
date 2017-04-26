c88e9d48b13786573fac86599a594209504ffc0e
Agnė Klimaitė <agnaite@me.com>
2017-04-26 15:39:45 -0700

    added template to carousel images

---
+++ js/templating.js
@@ -0,0 +1,40 @@
+$(function () {
+
+  //~~~~~~~ exhibits ~~~~~~~~
+
+  // var data = {"data": range(1, 42)};
+  // console.log(data);
+
+  Handlebars.registerHelper('times', function(n, block) {
+    var accum = '';
+    for(var i = 2; i<n; i++)
+        accum += block.fn(i);
+    return accum;
+  });
+
+  // Grab the template script
+  var theTemplateScript = $("#exhibit-carousel-img").html();
+  
+  // Compile the template
+  var theTemplate = Handlebars.compile(theTemplateScript);
+
+  // Pass our data to the template
+  // var theCompiledHtml = theTemplate(data);
+
+  // Add the compiled html to the page
+  $('.exhibit-img-placeholder').prepend(theTemplate);
+});
+
+
+// HELPERS
+
+function range(start, end)
+{
+    var dict = {images:[]};
+    
+    for(var i=start; i<end; i++)
+    {
+        dict.images.push(i);
+    }
+    return dict;
+}
