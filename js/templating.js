d8e03c563ac9e0a5cafc5d688b959f95ed6137fb
Agnė Klimaitė <agnaite@me.com>
2017-04-26 18:14:12 -0700

    now displays exhibit pics in correct order

--- js/templating.js
+++ js/templating.js
@@ -2,12 +2,9 @@ $(function () {
 
   //~~~~~~~ exhibits ~~~~~~~~
 
-  // var data = {"data": range(1, 42)};
-  // console.log(data);
-
   Handlebars.registerHelper('times', function(n, block) {
     var accum = '';
-    for(var i=2; i<n; i++)
+    for(var i=49; i>0; i--)
         if (i !== 23) { accum += block.fn(i); }
     return accum;
   });
@@ -18,23 +15,6 @@ $(function () {
   // Compile the template
   var theTemplate = Handlebars.compile(theTemplateScript);
 
-  // Pass our data to the template
-  // var theCompiledHtml = theTemplate(data);
-
   // Add the compiled html to the page
   $('.exhibit-img-placeholder').prepend(theTemplate);
 });
-
-
-// HELPERS
-
-function range(start, end)
-{
-    var dict = {images:[]};
-    
-    for(var i=start; i<end; i++)
-    {
-        dict.images.push(i);
-    }
-    return dict;
-}
